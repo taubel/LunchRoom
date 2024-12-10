df7f9eed009ccc182f0d29a522922a5e5b6d242c
Tautvydas Belgeras <tautvydas.b@8devices.com>
2024-12-10 20:41:11 +0200

    Add initial models

--- lunchroom_app/models.py
+++ lunchroom_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+# Model representing a 'Room'
+#  Room: a congregation of engineers ready to order some food
+class Room(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    # TODO decide what to put in 'on_delete'
+    patron = models.ForeignKey("User", on_delete=models.PROTECT)
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    rooms = models.ManyToManyField(Room)
+
+
+class FoodItem(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
