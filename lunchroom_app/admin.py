fd42f4df255f4428ca1333f6243319d22e2d6b42
Tautvydas Belgeras <tautvydas.b@8devices.com>
2024-12-10 20:49:32 +0200

    Add models to admin panel to allow testing

--- lunchroom_app/admin.py
+++ lunchroom_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Room, User, FoodItem
+
+
+admin.site.register(Room)
+admin.site.register(User)
+admin.site.register(FoodItem)
