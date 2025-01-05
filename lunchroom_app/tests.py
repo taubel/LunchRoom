5dacdcb4e76792262b1684c9f1f2b179475df577
Tautvydas Belgeras <tautvydas.b@8devices.com>
2025-01-05 18:21:42 +0200

    Add fooditem form tests

--- lunchroom_app/tests.py
+++ lunchroom_app/tests.py
@@ -1,4 +1,6 @@
+from bs4 import BeautifulSoup
 from django.test import TestCase
+from django.urls import reverse
 
 from . import models
 
@@ -20,3 +22,32 @@ class TestRoomModel(TestCase):
         food_item = models.FoodItem.objects.create(user=user, name="food_2", price=1)
         room.foods.add(food_item)
         assert len(room.users) == 2, "Room should find two users"
+
+    def test_fooditem_form_no_initial_room(self):
+        response = self.client.get(reverse("food-add"))
+
+        self.assertEqual(response.status_code, 200)
+
+        html = BeautifulSoup(response.content)
+        html_select = html.find("select", id="id_room")
+        self.assertIsNotNone(html_select)
+
+        html_option = html_select.find("option", value="")
+        self.assertIsNotNone(html_option)
+        self.assertTrue(html_option.has_attr("selected"))
+
+    # TODO test seems to fail because of no rooms defined in database
+    #  Prepare testing database
+    def test_fooditem_form_initial_room(self):
+        room_id = "5"
+        response = self.client.get(reverse("food-add"), query_params={"room_id": room_id})
+
+        self.assertEqual(response.status_code, 200)
+
+        html = BeautifulSoup(response.content)
+        html_select = html.find("select", id="id_room")
+        self.assertIsNotNone(html_select)
+
+        html_option = html_select.find("option", value=room_id)
+        self.assertIsNotNone(html_option)
+        self.assertTrue(html_option.has_attr("selected"))
